1b3c01a7e772824eed9938c848839787e3f1bf16
hudarasmey <huda.rasmey@gmail.com>
2020-04-11 07:21:32 +0200

    project created

---
+++ Source/EscapeRoom/EscapeRoomGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeRoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEROOM_API AEscapeRoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
